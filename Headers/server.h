6550f05518261c93804dfd5ea32326c18d95e845
Dancode52 <daniellanehart@gmail.com>
2026-02-02 16:10:50 +0100

    cleanup

--- Headers/server.h
+++ Headers/server.h
@@ -1,9 +1,6 @@
-// #define _POSIX_C_SOURCE 199309L
-// #define _XOPEN_SOURCE 600
-#include <unistd.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <signal.h>
-// #include <bits/sigaction.h>
-// #include <sys/types.h>
-
-//void	ft_usiputnbrcount_base_fd(size_t n, char *base, char *fill);
+#include "../libft/Headers/libft.h"
+#include <unistd.h>
+#include <stdio.h>
--- svr/server.c
+++ svr/server.c
@@ -1,9 +1,5 @@
-#include <stdint.h>
-#include <stdlib.h>
-#include <signal.h>
-#include "../libft/Headers/libft.h"
-#include <unistd.h>
-#include <stdio.h>
+
+#include "../Headers/server.h"
 
 char	*g_string;
 
@@ -16,7 +12,6 @@ char	*create_char(int signum, int *zerocount, int *bits)
 	if (*bits == 0)
 		c = 0;
 	c <<= 1;
-		//printf("made it into create char\n");
 	if (signum == SIGUSR1)
 	{
 		c |= 1;
@@ -28,14 +23,11 @@ char	*create_char(int signum, int *zerocount, int *bits)
 		*zerocount += 1;
 	}
 	*bits += 1;
-	//printf("bits: %i\n", *bits);
 	if (*bits == 8)
 	{
 		res = ft_calloc(2, sizeof(char));
 		res[0] = c;
 	}
-	// if (*zerocount > 8 && *bits != 8)
-	// 	*zerocount = 0;
 	return (res);
 }
 
@@ -44,10 +36,8 @@ char	*create_string(int	signum)
 	static int	zerocount;
 	static int	bits;
 	char 		*c;
-	//static char	*string;
 	char		*temp;
 
-		//printf("made it into create string\n");
 	temp = NULL;
 	c = create_char(signum, &zerocount, &bits);
 	if (bits == 8)
@@ -61,35 +51,10 @@ char	*create_string(int	signum)
 		g_string = ft_strdup(temp);
 		free(temp);
 		free(c);
-
-		// if (string)
-		// {
-		// 	write(1, "bitch work\n", 12);
-		// 	free(string);
-		// }
-		// if (string != NULL)
-		// {
-		// 	free(string);
-		// }
-		// if (string)
-		// {
-		// 	temp = ft_strdup(string);
-		// 	free(string);
-		// 	string = NULL;
-		// }
-		// if (temp)
-		// {
-		// 	string = ft_strjoin(temp, &c);
-		// 	free(temp);
-		// 	temp = NULL;
-		// }
-		// else
-		//  	string = ft_strjoin(NULL, &c);
 		bits = 0;
 		if (zerocount != 8)
 			zerocount = 0;
 	}
-	//printf("zerocount = %i\n", zerocount);
 	if (zerocount == 8 && bits == 0)
 	{
 		zerocount = 0;
@@ -101,21 +66,14 @@ char	*create_string(int	signum)
 void	print_string(int signum)
 {
 	char	*string;
-	//int		len;
 
-	//printf("made it into print string\n");
 	string = create_string(signum);
 	if (!string)
 		return ;
-	// len = 0;
-	// while (string[len])
-	// 	len++;
 	ft_putendl_fd(string, 1);
 	free(g_string);
 	g_string = NULL;
 	string = NULL;
-	// write(1, string, len);
-	// write(1, "\n", 1);
 	return ;
 }
 
@@ -123,9 +81,6 @@ void	handler(int signum, siginfo_t *info, void *ucontext_t)
 {
 	(void)ucontext_t;
 
-	//printf("%i\n", i);
-	//ft_putnbr_fd(lol, 1);
-	//write(1, "\n", 1);
 	print_string(signum);
 	kill(info->si_pid, SIGUSR1);
 }
