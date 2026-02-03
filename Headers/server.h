adffe5a662ce3c86d1430e47b6462ec1673c9f83
Dancode52 <daniellanehart@gmail.com>
2026-02-03 14:06:55 +0100

    normed

--- Headers/server.h
+++ Headers/server.h
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 08:43:20 by dlanehar          #+#    #+#             */
+/*   Updated: 2026/02/03 14:06:41 by dlanehar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <signal.h>
 #include "../libft/Headers/libft.h"
 #include <unistd.h>
-#include <stdio.h>
